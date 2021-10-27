5307af113e1d3cb7261403a6cdd3ea36cc813ad3
HeyBanditoz <banditoz@protonmail.com>
2021-10-26 23:16:59 -0600

    Minor code cleanup
    * Resolve some deprecated methods from Fabric and JDA
    * Remove dead code
    * Transform buildAttachmentsUrl from the Frankenstein it once was (seriously, what the hell was I thinking? I was on the blame I guess)

--- src/main/java/mixin/MixinMinecraftServer.java
+++ src/main/java/mixin/MixinMinecraftServer.java
@@ -2,10 +2,8 @@ package mixin;
 
 import main.Interweave;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.WorldGenerationProgressListener;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,22 +12,8 @@ import java.util.UUID;
 
 @Mixin(MinecraftServer.class)
 public abstract class MixinMinecraftServer {
-
-//    @Shadow public abstract int getMaxPlayerCount();
-
     @Inject(at = @At("RETURN"), method = "sendSystemMessage")
     public void copySystemMessageToDiscord(Text text, UUID uUID, CallbackInfo ci) {
         Interweave.sendMessage(text);
     }
-
-//    @Inject(at = @At("RETURN"), method = "prepareStartRegion")
-//    public void startServer(WorldGenerationProgressListener worldGenerationProgressListener, CallbackInfo ci) {
-//        Interweave.sendStartMessage();
-//        Interweave.setPlayers(0, getMaxPlayerCount());
-//    }
-//
-//    @Inject(at = @At("HEAD"), method = "shutdown")
-//    public void stopServer(CallbackInfo ci) {
-//        Interweave.sendStopMessageAndShutdown();
-//    }
 }
